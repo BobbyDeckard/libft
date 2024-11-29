67c591410063e86e85a1bb668e329bf01e553dbd
Ivo Meulemans <imeulema@c1r11s8.42lausanne.ch>
2024-11-29 11:37:01 +0100

    Added test files

---
+++ test.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+#include <fcntl.h>
+
+int main()
+{
+	int		fd;
+	int		iteration;
+	char	*line;
+
+	fd = open("file.txt", O_RDONLY);
+	line = "blabla";
+	iteration = 0;
+	while (line)
+	{
+		line = get_next_line(fd);
+		if (line)
+			ft_printf("Iteration %d, line: %sMemory address: %p\n", iteration, line, line);
+		free(line);
+	}
+	close(fd);
+	return 0;
+}
